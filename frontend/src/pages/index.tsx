8e99cd6b83c8199db7e49779b732e0b992dc2a06
n-daisuke <baseball.11.daisuke@gmail.com>
2023-08-27 20:16:09 +0900

    定期コミット

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,31 +1,34 @@
-import { Inter } from 'next/font/google';
-import { LoadingButton } from '../components/Button';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { Inter } from 'next/font/google'
+import { LoadingButton } from '../components/Button'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
-  const router = useRouter();
-  const [shouldRedirect, setShouldRedirect] = useState<boolean>(true);
+  const router = useRouter()
+  const [shouldRedirect, setShouldRedirect] = useState<boolean>(true)
 
   useEffect(() => {
-    const redirectTimer = setTimeout(()=>{
-      setShouldRedirect(false);
-      router.push('login/login', "login")
+    const redirectTimer = setTimeout(() => {
+      setShouldRedirect(false)
+      router.push('login/start', 'start')
     }, 1000)
 
     return () => {
-      clearTimeout(redirectTimer); // コンポーネントがアンマウントされる際にタイマーをクリアする
-    };
+      clearTimeout(redirectTimer) // コンポーネントがアンマウントされる際にタイマーをクリアする
+    }
   }, [])
 
   return (
     <>
       <div className="bg-white h-screen flex mx-auto w-full justify-center">
-        <div className='my-96'>
-          {shouldRedirect == true ? <LoadingButton></LoadingButton> : <div>Redirecting...</div>}
+        <div className="my-96">
+          {shouldRedirect == true ? (
+            <LoadingButton></LoadingButton>
+          ) : (
+            <div>Redirecting...</div>
+          )}
         </div>
       </div>
     </>
--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,22 +1,58 @@
-import Head from 'next/head';
-import React from 'react';
-import Image from 'next/image';
+import Head from 'next/head'
+import React from 'react'
+import Image from 'next/image'
+import backgroundImage from '../../../public/assets/backgroudPicture.png'
 
-const LoginPage = () => {
+const login = () => {
   return (
     <>
-     <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <button className="btn btn-primary">Get Started</button>
+      <div className="hero min-h-screen bg-base-200">
+        <Image src={backgroundImage} alt="card_game" layout="fill" />
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    create account??
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default login
