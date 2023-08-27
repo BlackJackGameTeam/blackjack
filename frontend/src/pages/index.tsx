2e556b3fc153244e6ed5b10c4205e39fbe6157ea
n-daisuke <baseball.11.daisuke@gmail.com>
2023-08-27 10:49:29 +0900

    定期コミット

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,31 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 import { LoadingButton } from '../components/Button';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  const router = useRouter();
+  const [shouldRedirect, setShouldRedirect] = useState<boolean>(true);
+
+  useEffect(() => {
+    const redirectTimer = setTimeout(()=>{
+      setShouldRedirect(false);
+      router.push('login/login', "login")
+    }, 1000)
+
+    return () => {
+      clearTimeout(redirectTimer); // コンポーネントがアンマウントされる際にタイマーをクリアする
+    };
+  }, [])
+
   return (
     <>
       <div className="bg-white h-screen flex mx-auto w-full justify-center">
         <div className='my-96'>
-          <LoadingButton></LoadingButton>
+          {shouldRedirect == true ? <LoadingButton></LoadingButton> : <div>Redirecting...</div>}
         </div>
       </div>
     </>
