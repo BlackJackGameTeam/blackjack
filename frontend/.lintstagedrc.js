3b6e3df80c67c9d488b99bbd638c070e0cbe8e87
n-daisuke <baseball.11.daisuke@gmail.com>
2023-08-31 22:19:08 +0900

    feat: ✨#14 huskyを導入する

---
+++ frontend/.lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "**/*.{js,jsx,ts,tsx}": [
+      filenames =>
+        `next lint --fix --file ${filenames
+          .map(file => file.split(process.cwd())[1])
+          .join(" --file ")}`,
+      "prettier --write",
+    ],
+    "**/*.{scss,css,sass}": ["stylelint --fix '**/*.{css,scss}'"],
+  };
